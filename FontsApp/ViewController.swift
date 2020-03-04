a319f50771930caf50c4981c2cf6ac7792530312
SangNX <nxsang063@gmail.com>
2020-03-04 12:54:12 +0700

    Initial Commit

---
+++ FontsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FontsApp
+//
+//  Created by Sang on 3/4/20.
+//  Copyright © 2020 SangNX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
